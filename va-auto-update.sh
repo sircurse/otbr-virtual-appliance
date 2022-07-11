40d9fa5285a4ce8f9158b56bce2f1f90d5265971
Curse <anderson@lalumail.com>
2022-07-11 13:05:28 +0200

    Fix to va-check-update-unix-format (#6)
    Fix to va-check-update-unix-format

--- va-auto-update.sh
+++ va-auto-update.sh
@@ -17,18 +17,19 @@ LOCAL_VA_PATCH=${LOCAL_VA_VER##*.}
 LOCAL_VA_MINOR=${LOCAL_VA_VER##$LOCAL_VA_MAJOR.}
 LOCAL_VA_MINOR=${LOCAL_VA_MINOR%%.$LOCAL_VA_PATCH}
 
-if (($LOCAL_VA_PATCH <= 17 )); then # Start update <= v1.0.17
-    wget -q -O /otbr/system/va-check-update.sh https://raw.githubusercontent.$REMOTE_VA/main/va-check-update.sh
-    chmod +x /otbr/system/va-check-update.sh
+if (($LOCAL_VA_MINOR <= 1 )); then # Start update <= v1.1.0
+    if (($LOCAL_VA_PATCH <= 0 )); then # Start update <= v1.1.1
+        wget -q -O /otbr/system/va-check-update.sh https://raw.githubusercontent.$REMOTE_VA/main/va-check-update.sh
+        chmod +x /otbr/system/va-check-update.sh
+        sed -i -e 's/\r$//' /otbr/system/va-check-update.sh
 
-    wget -q -O /otbr/system/auto-backup.sh https://raw.githubusercontent.$REMOTE_VA/main/auto-backup.sh
-    chmod +x /otbr/system/auto-backup.sh
+        wget -q -O /otbr/system/auto-backup.sh https://raw.githubusercontent.$REMOTE_VA/main/auto-backup.sh
+        chmod +x /otbr/system/auto-backup.sh
 
-    wget -q -O /otbr/system/server-repo-update.sh https://raw.githubusercontent.$REMOTE_VA/main/server-repo-update.sh
-    chmod +x /otbr/system/server-repo-update.sh
-fi # End update <= v1.0.17
-
-
+        wget -q -O /otbr/system/server-repo-update.sh https://raw.githubusercontent.$REMOTE_VA/main/server-repo-update.sh
+        chmod +x /otbr/system/server-repo-update.sh
+    fi # End update <= v1.1.1
+fi
 
 
 #pkill canary    # This line must be the last and can be uncommented only if a restart is required to apply changes.
