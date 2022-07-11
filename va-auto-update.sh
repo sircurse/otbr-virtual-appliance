a1626db4e829bb58c54f9c3a33e6bc6205ea70b4
Curse <anderson@lalumail.com>
2022-07-11 12:34:07 +0200

    Fix auto update part 2 (#5)
    Fix auto update part 2
    #ADD

--- va-auto-update.sh
+++ va-auto-update.sh
@@ -17,7 +17,7 @@ LOCAL_VA_PATCH=${LOCAL_VA_VER##*.}
 LOCAL_VA_MINOR=${LOCAL_VA_VER##$LOCAL_VA_MAJOR.}
 LOCAL_VA_MINOR=${LOCAL_VA_MINOR%%.$LOCAL_VA_PATCH}
 
-if (($LOCAL_VA_PATCH <= 16 )); then # Start update <= v1.0.15
+if (($LOCAL_VA_PATCH <= 17 )); then # Start update <= v1.0.17
     wget -q -O /otbr/system/va-check-update.sh https://raw.githubusercontent.$REMOTE_VA/main/va-check-update.sh
     chmod +x /otbr/system/va-check-update.sh
 
@@ -26,7 +26,7 @@ if (($LOCAL_VA_PATCH <= 16 )); then # Start update <= v1.0.15
 
     wget -q -O /otbr/system/server-repo-update.sh https://raw.githubusercontent.$REMOTE_VA/main/server-repo-update.sh
     chmod +x /otbr/system/server-repo-update.sh
-fi # End update <= v1.0.15
+fi # End update <= v1.0.17
 
 
 
