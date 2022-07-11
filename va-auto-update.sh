e2bd96b788990c8835f289b2cc372973e0a887d9
sircurse <anderson@lalumail.com>
2022-07-11 15:18:44 +0200

    New version update and fixed tags to bump version
    Added new way to test and update versions and fixed tags on bump version workflow

--- va-auto-update.sh
+++ va-auto-update.sh
@@ -9,27 +9,34 @@ SUFFIX=".git"
 
 
 # Check OTBR-VA Updates
-REMOTE_VA=${VA_REPO#"$PREFIX"}
-REMOTE_VA=${REMOTE_VA%"$SUFFIX"}
-LOCAL_VA_VER=$(grep '"version":' /otbr/system/server-build/package.json -m1 | cut -d\" -f4)
-LOCAL_VA_MAJOR=${LOCAL_VA_VER%%.*}
-LOCAL_VA_PATCH=${LOCAL_VA_VER##*.}
-LOCAL_VA_MINOR=${LOCAL_VA_VER##$LOCAL_VA_MAJOR.}
-LOCAL_VA_MINOR=${LOCAL_VA_MINOR%%.$LOCAL_VA_PATCH}
+MSORT_OK=$(dpkg-query -W --showformat='${Status}\n' msort|grep "install ok installed")
+if [ "" = "$MSORT_OK" ]; then
+  echo "No msort installed. Setting up msort."
+  apt install -y msort
+fi
 
-if (($LOCAL_VA_MINOR <= 1 )); then # Start update <= v1.1.0
-    if (($LOCAL_VA_PATCH <= 0 )); then # Start update <= v1.1.1
+# Save versions in variables
+REMOTE_VA_VER=$(grep '"version":' /otbr/system/tmp/va-version.json -m1 | cut -d\" -f4)
+LOCAL_VA_VER=$(grep '"version":' /otbr/system/server-build/package.json -m1 | cut -d\" -f4)
+
+# Function to verify if local version is less than remote version
+function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
+
+# Check versions
+if version_lt $LOCAL_VA_VER $REMOTE_VA_VER; then
+    if version_lt $LOCAL_VA_VER "1.1.2"; then # UPDATE v1.2.0
         wget -q -O /otbr/system/va-check-update.sh https://raw.githubusercontent.$REMOTE_VA/main/va-check-update.sh
         chmod +x /otbr/system/va-check-update.sh
         sed -i -e 's/\r$//' /otbr/system/va-check-update.sh
 
         wget -q -O /otbr/system/auto-backup.sh https://raw.githubusercontent.$REMOTE_VA/main/auto-backup.sh
         chmod +x /otbr/system/auto-backup.sh
+        sed -i -e 's/\r$//' /otbr/system/auto-backup.sh
 
         wget -q -O /otbr/system/server-repo-update.sh https://raw.githubusercontent.$REMOTE_VA/main/server-repo-update.sh
         chmod +x /otbr/system/server-repo-update.sh
-    fi # End update <= v1.1.1
+        sed -i -e 's/\r$//' /otbr/system/server-repo-update.sh
+    fi
 fi
-
 
 #pkill canary    # This line must be the last and can be uncommented only if a restart is required to apply changes.
